8938ffdfcc2741d2f0e1dab330b5a8dc0767b62f
Raccoon <a60814billy@gmail.com>
2016-03-29 05:48:08 +0800

    * add CSV test case

--- src/test/java/org/jfree/data/JFreeChartDataTestSuite.java
+++ src/test/java/org/jfree/data/JFreeChartDataTestSuite.java
@@ -1,11 +1,13 @@
 package org.jfree.data;
 
+import org.jfree.data.io.CSVTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        DataUtilitiesUnitTest.class
+        DataUtilitiesUnitTest.class,
+        CSVTest.class
 })
 public class JFreeChartDataTestSuite {
 }
