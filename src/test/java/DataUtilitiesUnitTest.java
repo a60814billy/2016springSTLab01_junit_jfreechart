fb70bb747711ec4b1d4e2a74e49594d542aff24e
Raccoon <a60814billy@gmail.com>
2016-03-28 23:46:53 +0800

    * use setup and teardown for testdata

--- src/test/java/DataUtilitiesUnitTest.java
+++ src/test/java/DataUtilitiesUnitTest.java
@@ -1,63 +1,71 @@
 import org.jfree.data.DataUtilities;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataUtilitiesUnitTest {
 
+    private FakeValues2D testVal;
+
+    @Before
+    public void setUp(){
+        this.testVal = new FakeValues2D();
+    }
+
+    @After
+    public void tearDown(){
+        this.testVal = null;
+    }
+
     //region test public static double calculateColumnTotal(Values2D data,int column)
     @Test
     public void testColumn0CalculateColumnTotal() {
-        FakeValues2D testVal = new FakeValues2D();
-        double result = DataUtilities.calculateColumnTotal(testVal, 0);
+        double result = DataUtilities.calculateColumnTotal(this.testVal, 0);
         assertEquals(6.0, result, 0.0001);
     }
 
     @Test
     public void testColumn2CalculateColumnTotal() {
-        FakeValues2D testVal = new FakeValues2D();
-        double result = DataUtilities.calculateColumnTotal(testVal, 2);
+        double result = DataUtilities.calculateColumnTotal(this.testVal, 2);
         assertEquals(24.0, result, 0.0001);
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
     public void testOverColumnIndexCalculateColumnTotal() {
-        FakeValues2D testVal = new FakeValues2D();
-        double result = DataUtilities.calculateColumnTotal(testVal, 3);
+        DataUtilities.calculateColumnTotal(this.testVal, 3);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testDataIsNullColumnIndexCalculateColumnTotal() {
-        FakeValues2D testVal = null;
-        double result = DataUtilities.calculateColumnTotal(testVal, 3);
+        this.testVal = null;
+        DataUtilities.calculateColumnTotal(this.testVal, 3);
     }
     //endregion
 
     //region test public static double calculateRowTotal(Values2D data,int row)
     @Test
     public void testRow0CalculateRowTotal() {
-        FakeValues2D testVal = new FakeValues2D();
-        double result = DataUtilities.calculateRowTotal(testVal, 0);
+        double result = DataUtilities.calculateRowTotal(this.testVal, 0);
         assertEquals(12.0, result, 0.0001);
     }
 
     @Test
     public void testRow2CalculateRowTotal() {
-        FakeValues2D testVal = new FakeValues2D();
-        double result = DataUtilities.calculateRowTotal(testVal, 2);
+        double result = DataUtilities.calculateRowTotal(this.testVal, 2);
         assertEquals(18.0, result, 0.0001);
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
     public void testRowOverIndexCalculateRowTotal() {
-        FakeValues2D testVal = new FakeValues2D();
-        double result = DataUtilities.calculateRowTotal(testVal, 3);
+        double result = DataUtilities.calculateRowTotal(this.testVal, 3);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testDataNullOnCalculateRowTotal() {
-        FakeValues2D testVal = null;
-        double result = DataUtilities.calculateRowTotal(testVal, 0);
+        this.testVal = null;
+        double result = DataUtilities.calculateRowTotal(this.testVal, 0);
     }
     //endregion
 
